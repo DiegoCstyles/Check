dc11981e717d908705ea42e6f6c4a30fe330de53
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-14 19:55:05 -0300

    Update NavbarPage.tsx

--- components/Navbar/NavbarPage.tsx
+++ components/Navbar/NavbarPage.tsx
@@ -1,17 +1,9 @@
 import Link from "next/link"
 
-interface SideNavbarProps {
-    page: string;
-    pageTitle: string;
-    className?: string;
-}
+interface SideNavbarProps { page: string; pageTitle: string; className?: string; }
 
 const NavbarPage = ({ page, pageTitle, className }: SideNavbarProps) => {
-    return (
-
-            <Link className={"text-xs p-2 "+ className} aria-current="page" href={"/"+ page}>{pageTitle.toUpperCase()}</Link>
-
-      )
+    return ( <Link className={"text-xs p-2 "+ className} aria-current="page" href={"/"+ page}>{pageTitle.toUpperCase()}</Link> )
 }
 
 export default NavbarPage
