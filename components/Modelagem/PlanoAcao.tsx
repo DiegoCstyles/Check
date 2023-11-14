491c5826647a97f3b0fe0d64b3086faf8d6010e6
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-14 19:42:43 -0300

    Update PlanoAcao.tsx

--- components/Modelagem/PlanoAcao.tsx
+++ components/Modelagem/PlanoAcao.tsx
@@ -6,35 +6,25 @@
   import ChecklistAplicados from './ChecklistAplicados';
 
   const ActionPlanPage: React.FC = () => {
-    const [chartData, setChartData] = useState<RiskItem[]>([]);
-    const [checklistOpen, setChecklistOpen] = useState<boolean>(false);
-
-    useEffect(() => {
-      fetchChartData();
-    }, []);
+    const [chartData, setChartData] = useState<RiskItem[]>([]); const [checklistOpen, setChecklistOpen] = useState<boolean>(false);
+    
+    useEffect(() => { fetchChartData(); }, []);
 
     const fetchChartData = async () => {
       try {
         const response = await fetch('https://checkend.onrender.com/api/chartData'); // Adjust the URL accordingly
 
-        if (response.ok) {
-          const data = await response.json();
-          setChartData(data);
-        } else {
-          console.error('Error fetching chart data');
-        }
-      } catch (error) {
-        console.error('Error:', error);
-      }
+        if (response.ok) { const data = await response.json(); setChartData(data);
+        } else { console.error('Error fetching chart data'); }
+      } catch (error) { console.error('Error:', error); }
     };
 
-    const toggleChecklist = () => {
-      setChecklistOpen(!checklistOpen);
-    };
+    const toggleChecklist = () => { setChecklistOpen(!checklistOpen); };
 
     return (
-      <div className='text-center w-full  text-xs'>
+      <div className='text-center w-full text-xs'>
           <h1 className='bg-cyan-300 text-black flex justify-center border-b p-2 text-sm uppercase font-semibold'>Planos de Ação</h1>
+        
           <div>
             <button
               className='cursor-pointer border-b-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
@@ -49,10 +39,8 @@
           }`}>
             {checklistOpen && <ChecklistAplicados />}
           </div>
-          <div className='flex mb-2'> 
-            <ActionPlanChart actionData={chartData} /> 
-            <ActionPlanConfirmation /> 
-          </div>
+        
+          <div className='flex mb-2'> <ActionPlanChart actionData={chartData} />  <ActionPlanConfirmation />  </div>
       </div>
     );
   };
