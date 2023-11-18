820377d54a3c6eb59411a7f4d04116cc39572a13
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-18 16:49:51 -0300

    Update PlanoAcao.tsx

--- components/Modelagem/PlanoAcao.tsx
+++ components/Modelagem/PlanoAcao.tsx
@@ -24,48 +24,43 @@
     const toggleChart = () => { setChartOpen(!chartOpen); };
 
     return (
-      <div className='text-center w-full text-xs'>
+      <div className='text-center w-full flex flex-col text-xs'>
           <h1 className='bg-cyan-300 text-black flex justify-center border-b p-2 text-sm uppercase font-semibold'>Planos de Ação</h1>
 
-          <div className='flex flex-row w-full'>
-            <div>
-              <button
-                className='cursor-pointer w-1/2 mt-2 border-b-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
-                onClick={toggleChecklist}
-              >
-                {checklistOpen ? 'Fechar Checklists Aplicados ⬆' : 'Abrir Checklists Aplicados ⬇'}
-              </button>
-           
-              <div className='flex flex-row'>
-                <div className={`transition-max-h duration-500 ease-in-out overflow-hidden ${
-                  checklistOpen ? 'max-h-screen' : 'max-h-0'
-                }`}>
-                  {checklistOpen && <ChecklistAplicados />}
-                </div>
-                
-                <div className={`transition-max-h duration-500 ease-in-out overflow-hidden ${
-                  chartOpen ? 'max-h-screen' : 'max-h-0'
-                }`}>
-                  {chartOpen && (
-                    <div className='flex flex-row'>
-                      <ActionPlanChart actionData={chartData} />
-                      <ActionPlanConfirmation />
-                    </div>
-                  )}
-                </div>
-              </div>
+          <div className='flex flex-row'>
+            <button
+              className='cursor-pointer w-1/2 mt-2 border-b-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
+              onClick={toggleChecklist}
+            >
+              {checklistOpen ? 'Fechar Checklists Aplicados ⬆' : 'Abrir Checklists Aplicados ⬇'}
+            </button>
+            <button
+                  className='cursor-pointer w-1/2 mt-2 border-b-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
+                  onClick={toggleChart}
+                >
+                  {chartOpen ? 'Fechar Avaliação de checklists ⬆' : 'Abrir Avaliação de checklists ⬇'}
+            </button>
+          </div>
+        
+          <div className='flex flex-row'>
+            <div className={`transition-max-h duration-500 ease-in-out overflow-hidden ${
+              checklistOpen ? 'max-h-screen' : 'max-h-0'
+            }`}>
+              {checklistOpen && <ChecklistAplicados />}
             </div>
             
-
-            <div>
-              <button
-                className='cursor-pointer w-1/2 mt-2 border-b-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
-                onClick={toggleChart}
-              >
-                {chartOpen ? 'Fechar Avaliação de checklists ⬆' : 'Abrir Avaliação de checklists ⬇'}
-              </button>
+            <div className={`transition-max-h duration-500 ease-in-out overflow-hidden ${
+              chartOpen ? 'max-h-screen' : 'max-h-0'
+            }`}>
+              {chartOpen && (
+                <div className='flex flex-row'>
+                  <ActionPlanChart actionData={chartData} />
+                  <ActionPlanConfirmation />
+                </div>
+              )}
             </div>
           </div>
+          
       </div>
     );
   };
