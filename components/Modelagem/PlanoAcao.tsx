2faef62afb2653f397dd4e94924e2f23d5bcf961
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-18 17:06:10 -0300

    Update PlanoAcao.tsx

--- components/Modelagem/PlanoAcao.tsx
+++ components/Modelagem/PlanoAcao.tsx
@@ -27,21 +27,22 @@
       <div className='text-center w-full text-xs'>
         <h1 className='bg-cyan-300 text-black flex justify-center border-b p-2 text-sm uppercase font-semibold'>Planos de Ação</h1>
         <div className='flex flex-row'>
-          <h2 className='p-1.5 text-xs text-white border-b-4'>Checklist</h2>
-          <div className='flex flex-col'>
-            <button
-              className='cursor-pointer h-1/2 border-b-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
-              onClick={toggleChecklist}
-            >
-              {checklistOpen ? 'Aplicados ⬆' : 'Aplicados ⬇'}
-            </button>
-            <button
-              className='cursor-pointer h-1/2 border-b-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
-              onClick={toggleChart}
-            >
-              {chartOpen ? 'Avaliação⬆' : 'Avaliação ⬇'}
-            </button>
-          </div>
+            <h2 className='p-1.5 text-xs text-center text-white border-r-4 h-full'>Checklist</h2>
+            <div className='flex flex-col h-full'>
+              <button
+                className='cursor-pointer h-1/2 border-r-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
+                onClick={toggleChecklist}
+              >
+                {checklistOpen ? 'Aplicados ⬆' : 'Aplicados ⬇'}
+              </button>
+              <button
+                className='cursor-pointer h-1/2 border-r-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
+                onClick={toggleChart}
+              >
+                {chartOpen ? 'Avaliação⬆' : 'Avaliação ⬇'}
+              </button>
+            </div>
+
         
           <div className='flex flex-row'>
             <div className={`transition-max-h duration-500 ease-in-out overflow-hidden ${
