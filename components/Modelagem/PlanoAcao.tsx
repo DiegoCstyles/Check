385592cccece83cc83cb02218c0f345f6519f5d2
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-18 15:28:57 -0300

    Update PlanoAcao.tsx

--- components/Modelagem/PlanoAcao.tsx
+++ components/Modelagem/PlanoAcao.tsx
@@ -24,23 +24,27 @@
     return (
       <div className='text-center w-full text-xs'>
           <h1 className='bg-cyan-300 text-black flex justify-center border-b p-2 text-sm uppercase font-semibold'>Planos de Ação</h1>
-        
-          <div>
-            <button
-              className='cursor-pointer border-b-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
-              onClick={toggleChecklist}
-            >
-              {checklistOpen ? 'Fechar Checklists Aplicados ⬆' : 'Abrir Checklists Aplicados ⬇'}
-            </button>
-          </div>
+
+          <div className='flex flex-row'>
+            <div>
+              <button
+                className='cursor-pointer border-b-2 border text-white p-2 w-full hover:bg-white hover:border-black/80 hover:text-black'
+                onClick={toggleChecklist}
+              >
+                {checklistOpen ? 'Fechar Checklists Aplicados ⬆' : 'Abrir Checklists Aplicados ⬇'}
+              </button>
+            </div>
   
-          <div className={`transition-max-h duration-500 ease-in-out overflow-hidden ${
-            checklistOpen ? 'max-h-screen' : 'max-h-0'
-          }`}>
-            {checklistOpen && <ChecklistAplicados />}
+            <div>
+              <div className={`transition-max-h duration-500 ease-in-out overflow-hidden ${
+                checklistOpen ? 'max-h-screen' : 'max-h-0'
+              }`}>
+                {checklistOpen && <ChecklistAplicados />}
+              </div>
+            
+              <div className='flex mb-2'> <ActionPlanChart actionData={chartData} />  <ActionPlanConfirmation />  </div>
+            </div>
           </div>
-        
-          <div className='flex mb-2'> <ActionPlanChart actionData={chartData} />  <ActionPlanConfirmation />  </div>
       </div>
     );
   };
