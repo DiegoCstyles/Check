fe2b4efaa0b318e8598e49ed7a1efce90290e4ab
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-18 23:45:25 -0300

    fixing models

---
+++ components/Modelagem/models.ts
@@ -0,0 +1,28 @@
+// models.ts
+export interface RiskItem {
+  id: number;
+  title: string;
+  description: string;
+  plandescription: string;
+  planFiles: Buffer | string;
+  planFilesName: string;
+  planapproval: string;
+  likelihood: string;
+  impact: string;
+  date: string;
+  responsibleChecklist: string;
+  responsiblePlan: string;
+  completed: boolean;
+}
+
+export interface AppliedChecklist {
+  id: number;
+  title: string;
+  dateapplied: string;
+  // Add other properties relevant to applied checklists
+}
+
+export interface ActionPlan {
+  description: string;
+  count: number;
+}
