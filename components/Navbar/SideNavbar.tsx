74834410989b102e7cdc21a704ec42114f181519
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-20 18:27:22 -0300

    Update SideNavbar.tsx

--- components/Navbar/SideNavbar.tsx
+++ components/Navbar/SideNavbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <nav className='w-full flex'>
-      <div className="flex flex-col w-20 h-full bg-white/10 transition-opacity duration-300 hover:opacity-100 hover:w-40 group  opacity-60">
+      <div className="flex flex-col w-20 h-screen bg-white/10 transition-opacity duration-300 hover:opacity-100 hover:w-40 group opacity-60">
         <p className="p-2 m-1 text-center dark:text-cyan-300 font-black text-base tracking-tight">CHECK.</p>
         <div className='group-hover:visible invisible flex flex-col flex-grow h-full'>
           <div
