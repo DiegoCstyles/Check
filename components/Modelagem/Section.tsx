a55a1a4382062c97bfe48d83accb673f8d9df2ab
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-29 15:58:39 -0300

    Update Section.tsx

--- components/Modelagem/Section.tsx
+++ components/Modelagem/Section.tsx
@@ -287,10 +287,10 @@ useEffect(() => {
             )}
           </div>
           <div className='w-2/3 bg-black/10 border'>
-            <section className='text-center text-sm text-cyan-300 border-b-4 bg-slate-500/30  p-1.5 uppercase'>Assistente 🤖</section>
-              <p className="h-full text-white text-xs bg-white/10 text-justify p-3 border-t-4">
+            <section className='text-center text-sm text-cyan-300 border-b-4 bg-slate-500/30  p-1.5 uppercase'>Assistente</section>
+              <p className="h-full text-white text-xs bg-white/10 text-justify p-3">
                 {scenario  && (
-                  <ul className="text-white">
+                  <ul className="text-white h-32 bg-white/30 border-t-4">
                     {scenario.split(/•|-/).map((item, index) => (
                       <li key={index}>{item.trim()}</li>
                     ))}
