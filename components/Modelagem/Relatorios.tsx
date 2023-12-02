831b7edfde2a43fff3921f6f587fc38f71f2b5b9
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-02 14:25:07 -0300

    Update Relatorios.tsx

--- components/Modelagem/Relatorios.tsx
+++ components/Modelagem/Relatorios.tsx
@@ -76,6 +76,27 @@ const AppliedChecklistsChart = () => {
         } else { console.error('Error fetching risk item from the database'); }
       } catch (error) { console.error('Error:', error); }
     };
+
+    const addresultAppliedChecklistsToBackend = async (checklistId, results) => {
+    try {
+      const response = await fetch(`https://checkend.onrender.com/api/updateChecklistResults/${checklistId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ results }),
+      });
+
+      if (response.ok) {
+        console.log('Results submitted successfully');
+        // You can add additional logic here if needed
+      } else {
+        console.error('Error submitting results');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
     
     const getUsers = async () => {
       try {
@@ -270,6 +291,13 @@ const AppliedChecklistsChart = () => {
                   <select
                     className='bg-white/10 p-0.5'
                     placeholder='selecionar...'
+                    value={resultAppliedChecklists.results}
+                    onChange={(event) => {
+                      setResultAppliedChecklists({
+                        ...resultAppliedChecklists,
+                        results: event.target.value,
+                      });
+                    }}
                   >
                     <option className='bg-black' value="Sem resultados">Sem resultados</option>
                     <option className='bg-black' value="Parcial">Parcial</option>
@@ -277,11 +305,15 @@ const AppliedChecklistsChart = () => {
                   </select>
                 
               </div>
-              <button className='w-1/12 border p-1 border-b-4 bg-black hover:bg-white hover:border-black/80 hover:text-black'>Submeter</button>
+              <button className='w-1/12 border p-1 border-b-4 bg-black hover:bg-white hover:border-black/80 hover:text-black'  
+                onClick={() => addresultAppliedChecklistsToBackend(resultAppliedChecklists.id, resultAppliedChecklists.results)}
+              >
+                Submeter
+              </button>
             </div>
           </li>
         ))}
-        <div className='w-full bg-yellow-500 uppercase font-semibold text-black p-2'>Ultimos Checklists</div>
+        <div className='w-full bg-yellow-500 uppercase font-semibold text-black p-1 mt-2'>Ultimos Checklists</div>
         <div className='flex flex-row border-t-4 w-full h-full'>
           <div className='flex flex-col w-1/2 pr-1'>
             <div className='h-1/6 bg-black uppercase font-semibold p-1.5 flex justify-center mt-2'>❌ Reprovado</div>
