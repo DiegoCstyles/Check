8a120db3c0a70b263c4e7f709ff6ada90741fc79
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-02 08:41:08 -0300

    Update Relatorios.tsx

--- components/Modelagem/Relatorios.tsx
+++ components/Modelagem/Relatorios.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react'; import { Bar, PolarArea, Bubble } from 'react-chartjs-2';
-import { AppliedChecklist } from './models';
+import { AppliedChecklist, RiskItem } from './models';
 
 interface ChartData {
   labels: string[];
@@ -19,7 +19,24 @@ interface User {
   name: string;
 }
 
+const [newRisk, setNewRisk] = useState<RiskItem>({
+  id: 0,
+  title: '',
+  description: '',
+  plandescription: '',
+  planFiles: '', 
+  planFilesName: '',
+  planapproval: 'solução em análise',
+  likelihood: 'Pequena',
+  impact: 'Pequeno',
+  date: 'DD/MM/AAAA',
+  responsiblechecklist: '',
+  responsibleplan: '',
+  completed: false,
+});
+
 const AppliedChecklistsChart = () => {
+  const [riskItems, setRiskItems] = useState<RiskItem[]>([]);
 
   const [chartDataUserRanking, setChartDataUserRanking] = useState<ChartData>({
     labels: [],
@@ -32,6 +49,16 @@ const AppliedChecklistsChart = () => {
   });
 
   useEffect(() => {
+    const fetchRiskItems = async () => {
+      try {
+        const response = await fetch(`https://checkend.onrender.com/api/riskItemsLastApproval`);
+  
+        if (response.ok) {
+          const data = await response.json(); setRiskItems(data); // Update the riskItems state with the fetched data
+        } else { console.error('Error fetching risk item from the database'); }
+      } catch (error) { console.error('Error:', error); }
+    };
+    
     const getUsers = async () => {
       try {
         const response = await fetch('https://checkend.onrender.com/api/getUsers', {
@@ -71,6 +98,20 @@ const AppliedChecklistsChart = () => {
       }
     };
 
+    function formatDate(dateString: string | number | Date) {
+      // Parse the input date string
+      const inputDate = new Date(dateString);
+    
+      // Check if the input date is valid
+      if (isNaN(inputDate.getTime())) { return ''; }
+    
+      // Format the date as "DD/MM/YYYY"
+      const day = String(inputDate.getDate()).padStart(2, '0');
+      const month = String(inputDate.getMonth() + 1).padStart(2, '0');
+      const year = inputDate.getFullYear();
+    
+      return `${day}/${month}/${year}`;
+    }
 
     const fetchData = async () => {
       try { 
@@ -193,13 +234,65 @@ const AppliedChecklistsChart = () => {
         </div>
         <div className='w-full bg-yellow-500 uppercase font-semibold text-black p-2'>Ultimos Checklists</div>
         <div className='flex flex-row border-t-4 w-full h-80'>
-          <div className='flex flex-col w-1/2'>
+          <div className='flex flex-col w-1/2 pl-1'>
             <div className='h-1/6 bg-black uppercase font-semibold p-2 flex justify-center'>Reprovado</div>
             <div className='h-5/6 bg-red-500/50 p-2'>Valores</div>
           </div>
-          <div className='flex flex-col w-1/2'>
+          <div className='flex flex-col w-1/2 pr-1'>
             <div className='h-1/6 bg-black uppercase font-semibold p-2 flex justify-center'>Aprovado</div>
-            <div className='h-5/6 bg-green-500/50 p-2'>Valores</div>
+            <div className='h-5/6 bg-green-500/50 p-2'>
+              {riskItems
+                .map((risk) => (
+                  <li key={risk.id}>
+                    <div className='flex flex-col justify-between text-xs text-center p-4'>
+                      <div className="flex flex-row">
+                        <input
+                          className='w-1/2 text-center bg-white/10 border-b-4 m-2 p-2'
+                          type='text'
+                          value={risk.title}
+                        />
+                        <input
+                          className='w-1/2 text-center bg-white/10 border-b-4 m-2 p-2'
+                          type='text'
+                          value={risk.description}
+                        />
+                      </div>  
+                      <div className="flex flex-row">
+                        <input
+                          className='w-1/2 text-center bg-white/10 border-b-4 m-2 p-2'
+                          type='text'
+                          value={formatDate(risk.date)}
+                        />
+                        <select
+                          className='w-1/2 text-center bg-white/10 border-b-4 m-2 p-2'
+                          value={risk.impact}
+                        >
+                          <option className='bg-black border-b-4 m-2' value='Pequeno'>Pequeno</option>
+                          <option className='bg-black border-b-4 m-2' value='Medio'>Medio</option>
+                          <option className='bg-black border-b-4 m-2' value='Alto'>Alto</option>
+                        </select>
+                        <select
+                          className='w-1/2 text-center bg-white/10 border-b-4 m-2 p-2'
+                          value={risk.likelihood}
+                        >
+                          <option className='bg-black border-b-4 m-2' value='Pequena'> Pequena </option>
+                          <option className='bg-black border-b-4 m-2' value='Media'> Media </option>
+                          <option className='bg-black border-b-4 m-2' value='Alta'> Alta </option>
+                        </select>
+                        
+                        <a
+                          href={`https://checkend.onrender.com/api/downloadPlanFile/${risk.id}`}
+                          className='w-1/2 text-center bg-white/10 border-b-4 m-2 p-2 uppercase hover:bg-white hover:border-black/80 hover:text-black'  
+                          target='_blank'
+                          rel='noopener noreferrer'
+                        >
+                          Plano
+                        </a>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+            </div>
           </div>
         </div>
       </div>
