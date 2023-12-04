7dc400b68485698dd7f2d1bab625eb70c6d5aa49
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-04 15:21:11 -0300

    Update Section.tsx

--- components/Modelagem/Section.tsx
+++ components/Modelagem/Section.tsx
@@ -360,7 +360,7 @@ useEffect(() => {
         </div>
         
           <div className='flex flex-row bg-black/10 border justify-between h-1/2'>
-            <section className='text-center text-sm text-cyan-300 border-b-4 bg-slate-500/30' style={{ overflowY: 'auto', maxHeight: 'calc(100vh - 120px)' }}>
+            <section className='text-center text-sm text-cyan-300 border-b-4 bg-slate-500/30 w-1/3' style={{ overflowY: 'auto', maxHeight: 'calc(100vh - 120px)' }}>
               <h1 className='text-center p-1.5 text-xs text-black uppercase font-semibold text-black bg-yellow-500'>Ultimos checklists Criados</h1>
               <ul className='text-slate-400 mb-1 p-5'>
                 <div className='flex border-b-2 justify-center text-xs text-white'>
@@ -377,11 +377,11 @@ useEffect(() => {
                 ))}
               </ul>
             </section>
-            <section className='text-center text-sm text-cyan-300 border-b-4 bg-slate-500/30'>
+            <section className='text-center text-sm text-cyan-300 border-b-4 bg-slate-500/30 w-fit'>
               <h1 className='text-center p-1.5 text-xs text-black uppercase font-semibold text-black bg-yellow-500'>Resumo de riscos anuais</h1>
-              <canvas ref={canvasRef} id="lineGraph" width=400" height="200"></canvas>
+              <canvas ref={canvasRef} id="lineGraph" width="400" height="200"></canvas>
             </section>
-            <section className='text-center text-sm text-cyan-300 border-b-4 bg-slate-500/30' style={{ overflowY: 'auto', maxHeight: 'calc(100vh - 120px)' }}>
+            <section className='text-center text-sm text-cyan-300 border-b-4 bg-slate-500/30 w-1/3' style={{ overflowY: 'auto', maxHeight: 'calc(100vh - 120px)' }}>
               <h1 className='text-center p-1.5 text-xs text-black uppercase font-semibold text-black bg-yellow-500'>Historico de checklists</h1>
               <ul className='text-slate-400 mb-1 p-5'>
                 <div className='flex border-b-2 justify-center text-xs text-white'>
